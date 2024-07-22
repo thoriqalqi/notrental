4faa9e61cdb7a26bde43c9c9b81c9f42b82bcb91
thoriqalqi <thoriqalqi7@gmail.com>
2024-07-22 22:07:36 +0700

    update

---
+++ gallery.js
@@ -0,0 +1,15 @@
+//swipper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+  //navigation effect on scroll
